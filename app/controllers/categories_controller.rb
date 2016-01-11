9a1986e27c22e5af937f2032c415f5a8c2dbe156
Ava78 <chanie1981@gmail.com>
2016-01-11 18:41:28 +0100

    Essai de la Gem Administrate

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,29 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
-  def new
+   def show
+    @categories = Category.all
+    @categories = Category.find(params[:id])
   end
-
-  def show
+  
+  def create
+    Category.create title: params[:title], description: params[:description], category_id: params[:category_id]
+    redirect_to "/categories/index"
   end
 
   def update
+    @categories = Category.find(params[:id])
+    @categories.title = params[:title]
+    @categories.save
+    redirect_to "/categories/#{params[:id]}"
   end
 
   def destroy
+    @categories = Category.find(params[:id])
+    @categories.destroy
+    redirect_to "/categories"
   end
+
 end
